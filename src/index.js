b85374e52783b93fd2f99838c603f419e5806fbc
Александра Щетинина <aleksandrasetinina8@gmail.com>
2024-09-27 16:27:47 +0300

    init: start my first task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num = Math.abs(n);
+    let arr = num.toString().split("");
+    let str = arr.reverse();
+    let resalt = Number(str.join(""));
+    return resalt;
+};
